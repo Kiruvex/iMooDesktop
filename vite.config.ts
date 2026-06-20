f8014972890f091e14f53ef06e8605e953a03509
Kiruvex <3038929349@qq.com>
2026-06-20 14:29:23 +0800

    v1.1.0

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
           build: {
             outDir: 'dist-electron',
             rollupOptions: {
-              external: ['electron', 'electron-log', 'electron-store', 'iconv-lite'],
+              external: ['electron', 'electron-log', 'electron-store', 'iconv-lite', 'usb'],
             },
           },
         },
